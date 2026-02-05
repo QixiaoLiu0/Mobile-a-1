649ce9712e8c66cdeebc4eadcb45804042a8fad0
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-02-05 12:34:02 -0700

    Avatar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,14 +1,34 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleProp, StyleSheet, View, ViewStyle } from "react-native";
 
-const Avatar = () => {
+interface AvatarProps {
+  uri: string;
+  width: number;
+  style?: StyleProp<ViewStyle>;
+}
+
+const Avatar = ({ uri, width, style }: AvatarProps) => {
   return (
-    <View>
-      <Image />
+    <View style={[styles.avatarContainer, style]}>
+      <Image
+        source={{ uri: uri }}
+        style={[
+          styles.avatarImage,
+          { width: width, height: width, borderRadius: width / 2 },
+        ]}
+      />
     </View>
   );
 };
 
 export default Avatar;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  avatarContainer: {
+    overflow: "hidden",
+  },
+  avatarImage: {
+    resizeMode: "cover",
+    overflow: "hidden",
+  },
+});
