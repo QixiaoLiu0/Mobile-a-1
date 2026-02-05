14569a19366fea829b13067138a164ff566c524c
Tianao0110 <123719125+Tianao0110@users.noreply.github.com>
2026-02-05 12:08:57 -0700

    tabbar

--- app/components/TabBar.tsx
+++ app/components/TabBar.tsx
@@ -1,10 +1,30 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-const TabBar = () => {
-  return <View>TabBar</View>;
-};
+const TabBar = () => (
+  <View style={styles.container}>
+    <Feather name="home" size={28} color="black" />
+    <Feather name="search" size={28} color="black" />
+    <AntDesign name="play-circle" size={28} color="black" />
+    <Ionicons name="briefcase-outline" size={28} color="black" />
+    <MaterialCommunityIcons name="face-man-profile" size={28} color="black" />
+  </View>
+);
 
 export default TabBar;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
+    borderTopWidth: 1,
+    marginTop: 10,
+    paddingTop: 20,
+  },
+});
+
