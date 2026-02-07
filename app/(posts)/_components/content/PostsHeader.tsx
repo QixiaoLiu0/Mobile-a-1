8660bca33a4ee558b6b84ac2d360e01828e750aa
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2026-02-06 18:42:04 -0700

    Completed the Post Header

--- app/(posts)/_components/content/PostsHeader.tsx
+++ app/(posts)/_components/content/PostsHeader.tsx
@@ -1,10 +1,37 @@
+import Avatar from "@/app/components/Avatar";
+import AvatarDesc from "@/app/components/AvatarDesc";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
 const PostsHeader = () => {
-  return <View>PostHeader</View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Avatar uri="https://i.pravatar.cc/150?img=2" width={35} />
+        <View style={{ marginLeft: 10 }}>
+          <AvatarDesc />
+        </View>
+      </View>
+      <AntDesign style={styles.icon} name="ellipsis" size={22} color="black" />
+    </View>
+  );
 };
 
 export default PostsHeader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+  },
+  main: {
+    flexDirection: "row",
+  },
+  icon: {
+    position: "relative",
+    top: 8,
+  },
+});
