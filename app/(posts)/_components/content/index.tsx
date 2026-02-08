dc78dcb1b7e699d4162c07e5402de4920b7a46f4
Vishavjeet Singh <vishavjeetgill133@gmail.com>
2026-02-08 10:44:45 -0700

    Completed index.tsx using View.

--- app/(posts)/_components/content/index.tsx
+++ app/(posts)/_components/content/index.tsx
@@ -8,8 +8,17 @@ const ContentIndex = () => {
   return (
     <View>
       <PostsHeader />
-      <View>
-        <Image />
+      <View
+        style={{
+          width: "100%",
+          aspectRatio: 9 / 10,
+          overflow: "hidden",
+        }}
+      >
+        <Image
+          source={require("@/assets/images/ootd1.jpg")}
+          style={{ width: "100%", height: "100%" }}
+        />
       </View>
       <FeatureBtns />
       <Comments />
